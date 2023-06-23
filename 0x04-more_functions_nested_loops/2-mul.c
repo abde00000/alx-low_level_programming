2e693c9d69a2f057cb63beed3cb283d6cbaa5914
abdessamad <abdomahboub1999@gmail.com>
2023-06-23 02:11:03 +0000

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,6 @@
  * print_alphabet_x10 - function that will print the alphabet 10 times
  *
 */
-
 void print_alphabet_x10(void)
 {
 	int line, ch;
@@ -12,9 +11,7 @@ void print_alphabet_x10(void)
 	for (line = 0; line <= 9; ++line)
 	{
 		for (ch = 'a'; ch <= 'z'; ++ch)
-		{
 			_putchar(ch);
-		}
 		_putchar('\n');
 	}
 }
