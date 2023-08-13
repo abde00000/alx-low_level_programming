ddc7ae5a4b957271592fcf9164799a3c7c810c03
abdessamad <abdessamadelmahboub@gmail.com>
2023-08-13 12:06:53 +0000

    updated 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,21 +20,28 @@ char *str_concat(char *s1, char *s2)
 
 	i = 0;
 	while (s1[i] != '\0')
+	{
 		len1++;
 		i++;
+	}
 	i = 0;
 	while (s2[i] != '\0')
+	{
 		len2++;
 		i++;
+	}
 	len = len1 + len2;
 	ptr = malloc(sizeof(char) * len + 1);
 	if (ptr == NULL)
-		return (ULL);
+		return (NULL);
 	for (i = 0; i < len1; i++)
+	{
 		ptr[i] = s1[i];
-
+	}
 	for (; i < len; i++)
+	{
 		ptr[i] = s2[i - len1];
+	}
 	ptr[i] = '\0';
 	return (ptr);
 }
