98fa042c50e3c584e5a6db2c3faae0ea8b89dd63
abdessamad <abdessamadelmahboub@gmail.com>
2023-08-31 22:33:58 +0000

    _prime_helper

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@
 int is_prime_number(int n)
 {
 	if (n <= 1)
-		return (-1);
+		return (0);
 	return (_prime_helper(n, 2));
 }
 /**
@@ -24,5 +24,5 @@ int _prime_helper(int n, int divisor)
 		return (1);
 	if (n % divisor == 0)
 		return (0);
-	return (is_prime_number(n, divisor + 1));
+	return (_prime_helper(n, divisor + 1));
 }
