88ff5b4fa0fb2ac862571d04f12dbe0e0c0fcf60
abdessamad <abdessamadelmahboub@gmail.com>
2023-09-04 20:30:33 +0000

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 		len++;
 		i++;
 	}
-	ptr = (char *)malloc(sizeof(char) * (len - 1));
+	ptr = (char *)malloc(sizeof(char) * len);
 	if (ptr == NULL)
 	{
 		return (NULL);
