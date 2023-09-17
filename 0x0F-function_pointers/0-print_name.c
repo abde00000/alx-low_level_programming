8e3b97823e6706a6870e4d5369331c3a251812a8
abdessamad <abdessamadelmahboub@gmail.com>
2023-09-17 13:59:48 +0000

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - prints a name
  * @name: the name to be printed
- * @(*f)(char *): pointer to a function
+ * @f: pointer to a function
  */
 void print_name(char *name, void (*f)(char *))
 {
