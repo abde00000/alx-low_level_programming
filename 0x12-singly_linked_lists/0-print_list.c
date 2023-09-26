19253e109288a67578788e174d0ccb0f728bb40e
abdessamad <abdessamadelmahboub@gmail.com>
2023-09-26 22:46:15 +0000

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: points to the list_t
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int nodes = 0;
+
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	while (h != NULL)
+	{
+		printf("[%u] (%s)\n)", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this is readme file of : 0x12-singly_linked_lists 
