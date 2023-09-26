abb1542570b1f5edcd0997791eb78d2c26a9eddb
abdessamad <abdessamadelmahboub@gmail.com>
2023-09-26 23:24:20 +0000

    add head

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: the head of the list
@@ -17,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 		return NULL;
 
 	new->str = strdup(str);
-	new->len = len
+	new->len = len;
 	new->next = *head;
 
 	*head = new;
