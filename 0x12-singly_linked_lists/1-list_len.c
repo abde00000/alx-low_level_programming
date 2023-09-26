cccbfa8769c8e74b80345e6459f1151af51bb4dc
abdessamad <abdessamadelmahboub@gmail.com>
2023-09-26 23:04:03 +0000

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - count num of elements
+ * @h: pointer to the structure
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	list_t *temp;
+	unsigned int count = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
