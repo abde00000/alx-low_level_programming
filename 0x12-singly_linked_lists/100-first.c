e78cf91b8001fa81597ef8bbc079df0b24786594
abdessamad <abdessamadelmahboub@gmail.com>
2023-09-27 00:26:09 +0000

    updated 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * before_main - prints a text before the execution of
  *			the main function
  */
-void before_main(void) __attribute__((constructor))
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n
 		I bore my house upon my back!\n");
