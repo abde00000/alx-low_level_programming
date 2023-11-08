b64d2502ad23d63e29d70ccf7cf8bf1d984c7af7
abdessamad <abdessamadelmahboub@gmail.com>
2023-11-08 01:33:20 +0000

    changes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+ize_t _strlen(const char *text);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
