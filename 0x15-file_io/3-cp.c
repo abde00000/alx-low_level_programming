cf04df7f667db172d7371f7aaaac52de8782be78
abdessamad <abdessamadelmahboub@gmail.com>
2023-11-12 15:26:49 +0000

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -67,6 +67,12 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	B_read = read(from, buffer, 1024);
+	if (B_read == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	while (B_read > 0)
 	{
 	B_write = write(to, buffer, B_read);
