9036f5a97fbf135c3ad9e1b47e3de4f04c00a956
abdessamad <abdessamadelmahboub@gmail.com>
2023-11-12 15:19:30 +0000

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -56,15 +56,15 @@ int main(int argc, char *argv[])
 	if (from == -1)
 	{
 		dprintf(STDERR_FILENO,
-                                "Error: Can't read from file %s\n", argv[1]);
+			"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to == -1)
 	{
 		dprintf(STDERR_FILENO,
-                                "Error: Can't write to %s", argv[2]);
-                        exit(99);
+			"Error: Can't write to %s", argv[2]);
+		exit(99);
 	}
 	B_read = read(from, buffer, 1024);
 	while (B_read > 0)
@@ -73,8 +73,8 @@ int main(int argc, char *argv[])
 		if (B_write != B_read)
 		{
 			dprintf(STDERR_FILENO,
-                                "Error: Can't write to %s", argv[2]);
-                        exit(99);
+				"Error: Can't write to %s", argv[2]);
+		exit(99);
 		}
 	B_read = read(from, buffer, 1024);
 	}
