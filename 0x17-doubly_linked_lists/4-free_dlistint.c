d4f9a6ab15093ecda15419dc5638b7116e7393ba
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-01-19 22:23:31 +0100

    free the linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,13 +5,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *current = head;
+	dlistint_t *next;
+
 	if (head == NULL)
 	{
 		return;
 	}
-	dlistint_t *current = head;
-	dlistint_t *next;
-
 	while (current != NULL)
 	{
 		next = current->next;
