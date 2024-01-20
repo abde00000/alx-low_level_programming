e198f819c2f10c1f8193b43f3bbaa27479e17e55
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-01-20 15:16:34 +0100

    new update lol

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,9 +2,8 @@
 /**
  * insert_dnodeint_at_index -  inserts a new node at a given position.
  * @h: the head of the linked list
- * @idx: the index
+ * @idk: the index
  * @n: the data
- * Return: the new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -14,9 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	if (*h != NULL)
 	{
 		temp = *h;
@@ -27,19 +24,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 	}
 	if (len < idx)
-	{
 		return (NULL);
-	}
 	new->n = n;
 	if (idx == 0)
 	{
 		add_dnodeint(h, n);
 	}
 	if (idx == len)
+	{
 		add_dnodeint_end(h, n);
+	}
 	Nprev = *h;
 	for (i = 0; i < idx - 1 && Nprev != NULL; i++)
+	{
 		Nprev = Nprev->next;
+	}
 	new->prev = Nprev;
 	Nnext = Nprev->next;
 	new->next = Nnext;
@@ -47,4 +46,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (Nnext != NULL)
 		Nnext->prev = new;
 	return (new);
-}
+}	
