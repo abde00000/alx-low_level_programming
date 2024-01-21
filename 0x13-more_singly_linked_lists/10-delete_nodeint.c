3f1595bdf323557128211d06b8d70fda567b64a3
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-01-21 18:07:34 +0100

    fiz betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,49 +7,44 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *temp, *prev;
-    unsigned int len = 0;
-    unsigned int i;
+	listint_t *temp, *prev;
+	unsigned int len = 0, i;
 
-    temp = *head;
-    while (temp != NULL)
-    {
-        len++;
-        temp = temp->next;
-    }
-    if (*head == NULL || len < index)
-    {
-        return (-1);
-    }
-    if (index == 0)
-    {
-        temp = *head;
-        *head = temp->next;
-        free(temp);
-        return (1);
-    }
-    else if (index == len)
-    {
-        temp = *head;
-        while (temp->next != NULL && temp->next->next != NULL)
-        {
-            temp = temp->next;
-        }
-        free(temp->next);
-        temp->next = NULL;
-        return (1);
-    }
-    else
-    {
-        temp = *head;
-        for (i = 0; i < index - 1 && temp->next != NULL; i++)
-        {
-            prev = temp;
-            temp = temp->next;
-        }
-        prev->next = temp->next;
-        free(temp);
-        return (1);
-    }
-    return (-1);
+	temp = *head;
+	while (temp != NULL)
+	{
+		len++;
+		temp = temp->next;
+	}
+	if (*head == NULL || len < index)
+		return (-1);
+	if (index == 0)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	else if (index == len)
+	{
+		temp = *head;
+	while (temp->next != NULL && temp->next->next != NULL)
+		temp = temp->next;
+	free(temp->next);
+	temp->next = NULL;
+	return (1);
+	}
+	else
+	{
+		temp = *head;
+		for (i = 0; i < index - 1 && temp->next != NULL; i++)
+		{
+			prev = temp;
+			temp = temp->next;
+		}
+		prev->next = temp->next;
+		free(temp);
+		return (1);
+	}
+	return (-1);
 }
